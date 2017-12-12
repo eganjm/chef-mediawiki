aec09fb775c158799a6212577853ac9fa9694172
eganjm <j.mike.egan@gmail.com>
2017-12-12 00:57:39 +0000

    test

--- recipes/database.rb
+++ recipes/database.rb
@@ -16,6 +16,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+#
 
 node.set['mysql']['server_root_password'] = node['mediawiki']['mysql_root_pw']
 
